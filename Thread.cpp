dc2ac1a5ec20da28ffec3fa5a1d1874313a7d04b
nivdror1 <nivdror1@gmail.com>
2017-03-23 14:01:31 +0200

    Thread class being added

--- Thread.cpp
+++ Thread.cpp
@@ -1,4 +1,53 @@
 //a instance of a thread
 
-Thread::Thread()
+#include "Thread.h"
 
+/**
+ * @brief a constructor
+ * @param tid the id of the thread
+ */
+Thread::Thread(int tid){
+	_tid=tid;
+	_runningTimes=0;
+	_currState= Spawn;
+}
+
+/**
+ * @brief get the thread id
+ * @return thread id
+ */
+const int Thread::getThreadId(){
+	return _tid;
+}
+
+/**
+ * @brief  get the number of times the thread has been to running mode
+ * @return the number of times the thread has been to running mode
+ */
+const int Thread::getRunningTimes(){
+	return _runningTimes;
+}
+
+/**
+ * @brief increase the number of times the thread was at running state
+ * @return the increased runningTimes
+ */
+int Thread::increaseRunning(){
+	_runningTimes+=1;
+	return _runningTimes;
+}
+
+/**
+ * change the state of the thread
+ */
+void Thread::changeStatus(State newState){
+	_currState=newState;
+}
+
+/**
+ * @brief get the state of the theard
+ * @return the state of the theard
+ */
+const State Thread::getStatus(){
+	return _currState;
+}
--- Thread.h
+++ Thread.h
@@ -1,12 +1,24 @@
-// a thread object
+// a class of a  thread
 
 #ifndef OS_EX2_THREAD_H
 #define OS_EX2_THREAD_H
 
+/**
+ * the set of state that the thread could be in
+ */
+enum State {Spawn,Ready,Running,Blocked, Terminated};
+
 class Thread{
 private:
-	/**
-	int tid;
+	/** a id of the thread*/
+	int _tid;
+
+	/** the number of times the thread was at running state*/
+	int _runningTimes;
+
+
+	/** the current state of the thread*/
+	State _currState;
 
 
 public:
@@ -20,25 +32,31 @@ public:
 	 * @brief get the thread id
 	 * @return thread id
 	 */
-	const getThreadId();
+	const int getThreadId();
 
 	/**
 	 * @brief  get the number of times the thread has been to running mode
 	 * @return the number of times the thread has been to running mode
 	 */
-	const getQuanta();
+	const int getRunningTimes();
+
+	/**
+	 * @brief increase the number of times the thread was at running state
+	 * @return the increased runningTimes
+	 */
+	int increaseRunning();
 
 	/**
 	 * change the state of the thread
 	 * @return
 	 */
-	changeStatus();
+	void changeStatus(State newState );
 
 	/**
 	 * @brief get the state of the theard
 	 * @return the state of the theard
 	 */
-	const getStatus();
+	const State getStatus();
 
 };
 
